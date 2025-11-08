0f04cc292b3a410e3971492200d148aeb024c3bf
csmit863 <csmit863@outlook.com>
2025-11-08 17:10:21 +1000

    refactor

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
-# Fault-Tolerant-Brewing-Stand
+# web3-mc-potions
+
+This project aims to replicate the functionality of a minecraft brewing stand using a solidity smart contract and a React app interface. Using smart contracts in this project allows users to own potions and ingredients as ERC1155 tokens in their own wallet.  
--- smart-contracts/src/BrewingStand.sol
+++ smart-contracts/src/BrewingStand.sol
@@ -1,8 +1,8 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.0;
 
-import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "openzeppelin-contracts/contracts/token/ERC1155/ERC1155.sol";
+import "openzeppelin-contracts/contracts/access/Ownable.sol";
 
 contract BrewingStand is ERC1155, Ownable {
 
